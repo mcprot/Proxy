cd19ed4cadb3e5b3fdc9ed054cfc59eda9982f74
Shaun Agostinho <shaunagostinho@gmail.com>
2020-07-17 16:57:08 -0700

    update panel url

--- src/main/java/mcprot/proxy/api/RemoteAPI.java
+++ src/main/java/mcprot/proxy/api/RemoteAPI.java
@@ -16,28 +16,28 @@ import java.util.List;
 public class RemoteAPI {
 
     public static Proxies.Response getProxies() {
-        HttpResponse<String> httpResponse = Unirest.get("http://localhost:3000/api/" + Main.getConfig().getApiKey() + "/proxies").asString();
+        HttpResponse<String> httpResponse = Unirest.get("https://panel.mcprot.com/api/" + Main.getConfig().getApiKey() + "/proxies").asString();
         Gson g = new Gson();
         Proxies.Response response = g.fromJson(httpResponse.getBody(), Proxies.Response.class);
         return response;
     }
 
     public static Analytics.Response getAnalytics() {
-        HttpResponse<String> httpResponse = Unirest.get("http://localhost:3000/api/" + Main.getConfig().getApiKey() + "/analytics").asString();
+        HttpResponse<String> httpResponse = Unirest.get("https://panel.mcprot.com/api/" + Main.getConfig().getApiKey() + "/analytics").asString();
         Gson g = new Gson();
         Analytics.Response response = g.fromJson(httpResponse.getBody(), Analytics.Response.class);
         return response;
     }
 
     public static Plans.Response getPlans() {
-        HttpResponse<String> httpResponse = Unirest.get("http://localhost:3000/api/" + Main.getConfig().getApiKey() + "/plans").asString();
+        HttpResponse<String> httpResponse = Unirest.get("https://panel.mcprot.com/api/" + Main.getConfig().getApiKey() + "/plans").asString();
         Gson g = new Gson();
         Plans.Response response = g.fromJson(httpResponse.getBody(), Plans.Response.class);
         return response;
     }
 
     public static Servers.Response getServers() {
-        HttpResponse<String> httpResponse = Unirest.get("http://localhost:3000/api/" + Main.getConfig().getApiKey() + "/servers").asString();
+        HttpResponse<String> httpResponse = Unirest.get("https://panel.mcprot.com/api/" + Main.getConfig().getApiKey() + "/servers").asString();
         Gson g = new Gson();
         Servers.Response response = g.fromJson(httpResponse.getBody(), Servers.Response.class);
         return response;
@@ -46,7 +46,7 @@ public class RemoteAPI {
     public static void putConnection(List<Connection> connection) {
         Gson g = new Gson();
         //System.out.println(g.toJson(new Connection.Connections(connection)));
-        HttpResponse<String> httpResponse = Unirest.put("http://localhost:3000/api/" + Main.getConfig().getApiKey() + "/connection")
+        HttpResponse<String> httpResponse = Unirest.put("https://panel.mcprot.com/api/" + Main.getConfig().getApiKey() + "/connection")
                 .header("Content-Type", "application/json")
                 .body(g.toJson(new Connection.Connections(connection))).asString();
 
@@ -56,7 +56,7 @@ public class RemoteAPI {
     public static void putAnalytic(List<Analytic> analytics) {
         Gson g = new Gson();
         System.out.println(g.toJson(new Analytic.Analytics(analytics)));
-        HttpResponse<String> httpResponse = Unirest.put("http://localhost:3000/api/" + Main.getConfig().getApiKey() + "/analytic")
+        HttpResponse<String> httpResponse = Unirest.put("https://panel.mcprot.com/api/" + Main.getConfig().getApiKey() + "/analytic")
                 .header("Content-Type", "application/json")
                 .body(g.toJson(new Analytic.Analytics(analytics))).asString();
         System.out.println(httpResponse.getBody());
